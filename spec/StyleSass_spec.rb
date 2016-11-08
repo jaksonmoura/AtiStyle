4b92ae5056bf18c9bfcbda2f9170d2734f29cea5
Alinuxfriend <ajoshi856@gmail.com>
2016-11-08 18:45:22 +0530

    Modified assets dir

--- spec/StyleSass_spec.rb
+++ spec/StyleSass_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'rails'
 
-describe Style_Sass do
+describe StyleSass do
   it 'has a version number' do
     expect(StyleSass::VERSION).not_to be nil
   end
--- spec/install_generator_spec.rb
+++ spec/install_generator_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require 'generators/style_sass/install_generator'
 
 
-describe StyleSass::Generators::InstallGenerator do
+describe StyleSass::Generators::Install_Generator do
     it 'copies the directory to app/assets'
     it 'verifies the directory'
 end
