08c72d7ebb07713750ae1711227654c9878bf4fa
Jakson Moura <jaksonrochelly@Jaksons-MacBook-Pro.local>
2019-06-24 16:38:49 -0300

    Adição inicial 2

--- lib/generators/ati_style/install_generator.rb
+++ lib/generators/ati_style/install_generator.rb
@@ -4,8 +4,9 @@ module AtiStyle
       source_root File.expand_path("../../../..",__FILE__)
 
       def copy_views
-        directory 'app/assets/', 'app/assets/'
+        directory 'app/', 'app/'
         directory 'public/', 'public/'
+        directory 'templates/', 'templates/'
       end
 
     end
